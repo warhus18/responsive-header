5f47ca082c72cc9c3869a9c47a8dfd994a1c6b07
warhus18 <jessica.warhus@gmail.com>
2016-08-09 07:35:20 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php include("header.php"); ?>
 
 <div class="centered clearfix">
-    <h1>Simple Responsive Header</h1> 
+    <h1>Two-step Responsive Header</h1> 
     <p>Move the size of your browser window in and out to see the responsive header in action!</p>
 </div>
 
